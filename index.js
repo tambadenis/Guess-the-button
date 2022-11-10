8cbf78a1b9dfe98c3342b425c725c00c4c9d7a5f
tambadenis <56196003+tambadenis@users.noreply.github.com>
2022-11-10 09:43:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 let n = 3;
-      let winningButton;
-      const onClick = function() {
-        winningButton = winningButton ? winningButton : Math.floor(Math.random() * n);
-        if (winningButton == this.id) {
-          alert("Congratulations! You've guessed the button!")
-        }
-      }
-      for (let i = 1; i <= n; ++i) {
-        const buton = document.createElement("button")
-        document.querySelector("body").appendChild(buton)
-        buton.id = i
-        buton.innerText = i;
-        buton.addEventListener("click", onClick);
-      }
+let winningButton;
+
+const onClick = function() {
+  if (winningButton == this.id) {
+    alert("Congratulations! You've guessed the button!")
+  } 
+  winningButton = Math.floor(Math.random() * n);
+}
+
+for (let i = 1; i <= n; ++i) {
+  const buton = document.createElement("button")
+  document.querySelector("body").appendChild(buton)
+  buton.id = i
+  buton.innerText = "Button" + " " + i;
+  buton.addEventListener("click", onClick);
+}
+
+winningButton = winningButton ? winningButton : Math.floor(Math.random() * n);
